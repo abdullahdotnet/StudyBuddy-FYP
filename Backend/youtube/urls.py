ce567f4b796a8e50f07b76a0574cf4b695227ac4
Abdullah Ashfaq <abdullahashfaq.ds@gmail.com>
2024-09-20 10:13:13 -0300

    Add YouTube Summary API

--- Backend/youtube/urls.py
+++ Backend/youtube/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ImageDataView
+from .views import ImageDataView, YoutubeSummaryView
 
 
 urlpatterns = [
     path('upload/', ImageDataView.as_view(), name='upload_image'),
+    path('yt-summary/', YoutubeSummaryView.as_view(), name='yt_summary'),
 ]
