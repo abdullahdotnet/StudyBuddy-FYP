b41142b64260ba18d09b39d36e2c45b9a6551b3a
Abdullah Ashfaq <abdullahashfaq.ds@gmail.com>
2024-09-20 02:17:05 -0300

    Complete YT-Data Upload Views & Serializers

--- Backend/youtube/views.py
+++ Backend/youtube/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import ImageDataSerializer
 
-# Create your views here.
+
+class ImageDataView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = ImageDataSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
