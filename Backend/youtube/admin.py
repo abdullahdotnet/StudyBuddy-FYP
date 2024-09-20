bd70c8e88aed221313507f281d02b4a6d8b672f3
Abdullah Ashfaq <abdullahashfaq.ds@gmail.com>
2024-09-20 02:15:16 -0300

    Manage SuperUser Admin Panel YT

--- Backend/youtube/admin.py
+++ Backend/youtube/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ImageData
 
-# Register your models here.
+
+class ImageDataAdmin(admin.ModelAdmin):
+    list_display = ['id', 'timestamp', 'image', 'description']
+
+    class Meta:
+        verbose_name_plural = 'ImageData'
+
+
+admin.site.register(ImageData, ImageDataAdmin)
