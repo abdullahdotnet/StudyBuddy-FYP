01ee99a9e966c0781a4a15aeb4c9361da2a94820
anas-waleed-tahir <anastahir707070@gmail.com>
2024-09-24 20:33:50 +0500

    login page completed

--- Frontend/src/routes/routes.jsx
+++ Frontend/src/routes/routes.jsx
@@ -1,39 +1,43 @@
-import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import Main_Layout from '../layouts/MainLayout';
-import DashboardNav from '../layouts/DashboardNavbar';
-import Dashboard from '../pages/Dashboard/Dashboard';
-import Chatbot from '../pages/Chatbot/Chatbot';
-import NoNavbarLayout from '../layouts/NoNavbarLayout';
-import Home from '../pages/Home/Home';
-import Avatar from '../pages/Avatar/Avatar';
-import Resources from '../pages/Resources/Resources';
-import Extension from '../pages/Extension/Extension';
-import Extras from '../pages/Extras/Extras';
-import Extra2 from '../pages/Extras/Extras2';
-
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Main_Layout from "../layouts/MainLayout";
+import DashboardNav from "../layouts/DashboardNavbar";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Chatbot from "../pages/Chatbot/Chatbot";
+import NoNavbarLayout from "../layouts/NoNavbarLayout";
+import Home from "../pages/Home/Home";
+import Avatar from "../pages/Avatar/Avatar";
+import Resources from "../pages/Resources/Resources";
+import Extension from "../pages/Extension/Extension";
+import Extras from "../pages/Extras/Extras";
+import Extra2 from "../pages/Extras/Extras2";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <>
-        <Route path="/" element={<Main_Layout />} >
-            <Route path='' element={<Home />} />
-        </Route>
+  createRoutesFromElements(
+    <>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Main_Layout />}>
+        <Route path="" element={<Home />} />
+      </Route>
 
-        <Route path="/" element={<DashboardNav />} >
-            <Route path="chatbot" element={<Chatbot />} />
-            <Route path="/avatar" element={<Avatar />} />
-            <Route path="/resources" element={<Resources />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path='/extension' element={<Extension />} />
-            <Route path='/extras' element={<Extras />} />
-            <Route path="/extra2/:videoId" element={<Extra2 />} />
-            <Route path="*" element={<h1>Page Not found</h1>} />
-        </Route>
+      <Route path="/" element={<DashboardNav />}>
+        <Route path="chatbot" element={<Chatbot />} />
+        <Route path="/avatar" element={<Avatar />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/extension" element={<Extension />} />
+        <Route path="/extras" element={<Extras />} />
+        <Route path="/extra2/:videoId" element={<Extra2 />} />
+        <Route path="*" element={<h1>Page Not found</h1>} />
+      </Route>
 
-        <Route path='/' element={<NoNavbarLayout />} >
-        </Route>
-        </>
-    )
-)
+      <Route path="/" element={<NoNavbarLayout />}></Route>
+    </>
+  )
+);
 
-export default router
+export default router;
