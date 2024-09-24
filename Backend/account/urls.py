f4fec7160e9cca3270b7e2b8340e56cd7fdf018b
abdullah <abdullahdotnet20@gmail.com>
2024-09-24 23:24:41 +0500

    Login api done

--- Backend/account/urls.py
+++ Backend/account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from account.views import UserRegistrationView
+from account.views import UserLoginView
 
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register_user'),
+    path('login/', UserLoginView.as_view(), name='login')
 ]
