2843e0851da0ac0537b8c3c4f5d51bbd82c86fc6
abdullah <abdullahdotnet20@gmail.com>
2024-09-25 20:25:21 +0500

    change password Api

--- Backend/account/urls.py
+++ Backend/account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from account.views import UserRegistrationView
-from account.views import UserLoginView,UserProfileView
+from account.views import UserLoginView,UserProfileView,UserChangePasswordView
 
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register_user'),
     path('login/', UserLoginView.as_view(), name='login'),
-    path('profile/', UserProfileView.as_view(), name='profile')
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('changepassword/', UserChangePasswordView.as_view(), name='changepassword')
 ]
