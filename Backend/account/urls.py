f3b193015a532764726c5d998f90df941f9bdb95
Abdullah Ashfaq <abdullahashfaq.ds@gmail.com>
2024-09-27 14:26:00 +0500

    update code formatting & logic

--- Backend/account/urls.py
+++ Backend/account/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path, include
+from django.urls import path
 from account.views import UserRegistrationView
-from account.views import UserLoginView,UserProfileView,UserChangePasswordView,SendPasswordResetEmailView,UserPasswordResetView
-
+from account.views import (
+    EmailSend,
+    UserLoginView,
+    UserProfileView,
+    UserChangePasswordView,
+    SendPasswordResetEmailView,
+    UserPasswordResetView,
+)
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register_user'),
@@ -9,5 +15,6 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
     path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
-    path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password')
+    path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),
+    path('email-send/', EmailSend.as_view())
 ]
