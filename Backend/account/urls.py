ef73269d0aa9234a443f10f19e20ed07ae3fef7e
Abdullah Ashfaq <abdullahashfaq.ds@gmail.com>
2024-09-27 16:52:28 +0500

    update user account endpoints

--- Backend/account/urls.py
+++ Backend/account/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from account.views import UserRegistrationView
 from account.views import (
-    EmailSend,
     UserLoginView,
     UserProfileView,
     UserChangePasswordView,
@@ -13,8 +12,7 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register_user'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-    path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
-    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
-    path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),
-    path('email-send/', EmailSend.as_view())
+    path('change-password/', UserChangePasswordView.as_view(), name='change-password'),
+    path('reset-password/', SendPasswordResetEmailView.as_view(), name='reset-password'),
+    path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password-mail'),
 ]
