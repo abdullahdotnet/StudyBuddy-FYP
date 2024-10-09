cb321fba196294ea5cc9ec8dda410788c5db6fdb
abdullah <abdullahdotnet20@gmail.com>
2024-10-09 20:43:29 +0500

    changes in paper generation api

--- Backend/test_session/views.py
+++ Backend/test_session/views.py
@@ -20,3 +20,4 @@ class BoardPaperGenerationView(APIView):
             return Response(data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
