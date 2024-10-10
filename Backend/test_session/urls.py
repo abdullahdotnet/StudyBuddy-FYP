724d64e34ea2493c677d18b37485ad7239bf52fd
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-10-10 22:01:03 +0500

    commented the OCR code to remove errors

--- Backend/test_session/urls.py
+++ Backend/test_session/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import BoardPaperGenerationView
-from .views import BoardPaperEvaluation
+# from .views import BoardPaperGenerationView
+# from .views import BoardPaperEvaluation
 
 urlpatterns = [
     # path('/generate', TestSessionView.as_view(), name='generate'),
-    path('board-paper-generate/', BoardPaperGenerationView.as_view(), name='board-paper-generate'),
-    path('extract-text/', BoardPaperEvaluation.as_view(), name='extract-text')
+    # path('board-paper-generate/', BoardPaperGenerationView.as_view(), name='board-paper-generate'),
+    # path('extract-text/', BoardPaperEvaluation.as_view(), name='extract-text')
 ]
