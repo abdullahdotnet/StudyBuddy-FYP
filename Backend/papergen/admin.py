304401dfb619b7a91608679d8c20ccb955ae498c
Abdullah Ashfaq <abdullahashfaq.ds@gmail.com>
2024-10-11 17:15:34 +0500

    add subject model & admin

--- Backend/papergen/admin.py
+++ Backend/papergen/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Subject
 
-# Register your models here.
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'status', 'grade')
+    list_filter = ('status', 'grade')
+
+
+admin.site.register(Subject, SubjectAdmin)
--- Backend/papergen/models.py
+++ Backend/papergen/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    GRADE_CHOICES = [
+        ('9', 'Class 9'),
+        ('10', 'Class 10'),
+        ('11', 'Class 11'),
+        ('12', 'Class 12'),
+    ]
+
+    name = models.CharField(max_length=100)
+    status = models.BooleanField(default=True)
+    grade = models.CharField(max_length=2, choices=GRADE_CHOICES)
+
+    def __str__(self):
+        return self.name
