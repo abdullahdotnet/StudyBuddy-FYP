7b5e8a1dea30c91a4641de1dc92b1923a01d91c6
Abdullah Ashfaq <abdullahashfaq.ds@gmail.com>
2024-10-11 17:17:50 +0500

    complete subject api logic

--- Backend/papergen/views.py
+++ Backend/papergen/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Subject
+from .serializers import SubjectSerializer
 
-# Create your views here.
+
+class SubjectListView(APIView):
+    def get(self, request):
+        grade = request.query_params.get('grade', None)
+
+        if grade:
+            subjects = Subject.objects.filter(status=True, grade=grade)
+        else:
+            subjects = Subject.objects.filter(status=True)
+
+        serializer = SubjectSerializer(subjects, many=True)
+        return Response(serializer.data)
