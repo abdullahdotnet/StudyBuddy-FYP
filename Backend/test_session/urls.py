8bdee1048dc33f86fa2993a4269aa4d338cfdf8b
abdullah <abdullahdotnet20@gmail.com>
2024-10-11 18:23:13 +0500

    Paper evaluation api completed

--- Backend/test_session/urls.py
+++ Backend/test_session/urls.py
@@ -5,5 +5,5 @@ from .views import BoardPaperEvaluation
 urlpatterns = [
     # path('/generate', TestSessionView.as_view(), name='generate'),
     path('board-paper-generate/', BoardPaperGenerationView.as_view(), name='board-paper-generate'),
-    path('extract-text/', BoardPaperEvaluation.as_view(), name='extract-text')
+    path('evaluate-paper/', BoardPaperEvaluation.as_view(), name='evaluate-paper')
 ]
