e87735c86ac7ad7b6d5ffe8191089b8648a9b8d1
Abdullah Ashfaq <abdullahashfaq.ds@gmail.com>
2024-10-12 13:36:51 +0500

    complete api generate-paper

--- Backend/papergen/urls.py
+++ Backend/papergen/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SubjectListView
+from .views import SubjectListView, ObjectivePaperView
 
 urlpatterns = [
     path('subjects/', SubjectListView.as_view(), name='subjects'),
+    path('generate-paper/', ObjectivePaperView.as_view(), name='objective_questions'),
 ]
--- Backend/papergen/views.py
+++ Backend/papergen/views.py
@@ -1,7 +1,9 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Subject
 from .serializers import SubjectSerializer
+from .utils import generate_question_paper, split_questions
 
 
 class SubjectListView(APIView):
@@ -15,3 +17,23 @@ class SubjectListView(APIView):
 
         serializer = SubjectSerializer(subjects, many=True)
         return Response(serializer.data)
+
+
+class ObjectivePaperView(APIView):
+    def post(self, request):
+        papers = request.data.get('papers', 1)
+        try:
+            paper_text = generate_question_paper(papers)
+            questions = split_questions(paper_text[0])
+            return Response(
+                {
+                    "message": "Questions generated successfully.",
+                    "questions": questions
+                },
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                {"error": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
