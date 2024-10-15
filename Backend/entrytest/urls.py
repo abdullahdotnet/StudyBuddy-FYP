34ba877e394144e456789f6482c300834c6906bd
abdullah <abdullahdotnet20@gmail.com>
2024-10-16 00:28:31 +0500

    Entry test paper generation

--- Backend/entrytest/urls.py
+++ Backend/entrytest/urls.py
@@ -1,8 +1,6 @@
-# myapp/urls.py
-
 from django.urls import path
-from . import views
+from .views import GenerateQuestionPaperAPIView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('generate-entry-test/', GenerateQuestionPaperAPIView.as_view(), name='generate-entry-test')
 ]
