9daf918ec9e372c3befbc5332e8f775c453ae8c4
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-10-17 21:40:40 +0500

    resolving conflicts

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -11,12 +11,9 @@ urlpatterns = [
     path('api/user/', include('account.urls')),
     path('api/test-session/', include('test_session.urls')),
     path('api/userspace/', include('userspace.urls')),
-<<<<<<< HEAD
     path('api/papergen/', include('papergen.urls')),
-=======
     path('api/test-session/', include('papergen.urls')),
     path('api/entrytest/', include('entrytest.urls'))
->>>>>>> 3ea3171b765855d4976dc04f35a350cac58c897f
 ]
 
 if settings.DEBUG:
