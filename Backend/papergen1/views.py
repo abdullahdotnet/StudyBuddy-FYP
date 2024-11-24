26a2b0f60da3c66f5f576e6322ef2fac46ebf41d
etisamhaq <etisamulhaq2003@gmail.com>
2024-11-24 20:31:45 +0500

    add new paper gen folder

---
+++ Backend/papergen1/views.py
@@ -0,0 +1,69 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Subject
+from .serializers import SubjectSerializer
+from .utils import generate_question_paper, split_questions, evaluate_mcq_chain
+
+
+class SubjectListView(APIView):
+    def get(self, request):
+        grade = request.query_params.get('grade', None)
+
+        if grade:
+            subjects = Subject.objects.filter(status=True, grade=grade)
+        else:
+            subjects = Subject.objects.filter(status=True)
+
+        serializer = SubjectSerializer(subjects, many=True)
+        return Response(serializer.data)
+
+
+class ObjectivePaperView(APIView):
+    def post(self, request):
+        papers = request.data.get('papers', 1)
+        try:
+            paper_text = generate_question_paper(papers)
+            questions = split_questions(paper_text[0])
+            return Response(
+                {
+                    "message": "Questions generated successfully.",
+                    "questions": questions
+                },
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                {"error": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+class ObjectiveEvaluationView(APIView):
+    def post(self, request, grade, subject):
+        try:
+            questions = request.data.get("questions", None)
+
+            if not questions:
+                return Response(
+                    {"error": "No question data provided."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            evaluation_result = evaluate_mcq_chain(
+                questions, grade, subject
+            )
+
+            return Response(
+                {
+                    "message": "Evaluation completed.",
+                    "result": evaluation_result
+                },
+                status=status.HTTP_200_OK
+            )
+
+        except Exception as e:
+            return Response(
+                {"error": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
