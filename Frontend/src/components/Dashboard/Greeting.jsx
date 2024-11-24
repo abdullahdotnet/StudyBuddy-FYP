c7ad36d58a6cb2e319fb2e229bd0eca0c05226d9
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-11-24 16:43:39 +0500

    updated leaderboard and streak

--- Frontend/src/components/Dashboard/Greeting.jsx
+++ Frontend/src/components/Dashboard/Greeting.jsx
@@ -14,20 +14,24 @@ const Greeting = ({ name, quote, streak }) => {
       {/* Streak */}
       <div className="flex flex-col items-center mt-4">
         <div className="flex items-center text-lg font-medium text-gray-700">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 text-blue-600 mr-2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13.5 4.5L20.25 16.5H6.75L13.5 4.5z"
-            />
+          <svg width="40" height="40" viewBox="0 0 39 66" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_d_486_3543)">
+              <path d="M30.8624 6.52687C31.9038 4.75092 31.2616 2.04173 29.7681 1.92613L18.1854 1.02957C17.872 1.00157 17.5532 1.10503 17.2596 1.32998C16.966 1.55493 16.7074 1.89382 16.5088 2.31405L5.91117 25.3163C5.07522 27.1279 5.76431 29.5224 7.15265 29.6298L13.0665 30.0876L5.74762 52.2421C4.7211 55.0363 6.69537 57.8176 8.31784 55.864L20.9665 39.3706L33.6151 22.8771L21.8029 21.9628L30.8624 6.52687Z" stroke="#4255FF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" shape-rendering="crispEdges" />
+            </g>
+            <defs>
+              <filter id="filter0_d_486_3543" x="0.464844" y="0.0253906" width="38.1504" height="65.4932" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                <feOffset dy="4" />
+                <feGaussianBlur stdDeviation="2" />
+                <feComposite in2="hardAlpha" operator="out" />
+                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_486_3543" />
+                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_486_3543" result="shape" />
+              </filter>
+            </defs>
           </svg>
+
           {streak} Days Streak
         </div>
       </div>
@@ -38,11 +42,10 @@ const Greeting = ({ name, quote, streak }) => {
           {["M", "T", "W", "T", "F", "S", "S"].map((day, index) => (
             <div
               key={index}
-              className={`w-8 h-8 flex items-center justify-center rounded-full ${
-                index === 2 || index === 3 || index === 5
+              className={`w-8 h-8 flex items-center justify-center rounded-full ${index === 4 || index === 6 || index === 5
                   ? "bg-blue-600 text-white"
                   : "bg-gray-200 text-gray-500"
-              }`}
+                }`}
             >
               {day}
             </div>
--- Frontend/src/pages/Dashboard/Dashboard.jsx
+++ Frontend/src/pages/Dashboard/Dashboard.jsx
@@ -7,8 +7,10 @@ import TaskTable from "../../components/Dashboard/TaskTable";
 import Greeting from "../../components/Dashboard/Greeting";
 import Leaderboard from "../../components/Dashboard/Leaderboard";
 import Chats from "../../components/Dashboard/Chatbot";
+const username = sessionStorage.getItem("username");
+
 const user = {
-  name: "Anas",
+  name: username,
   quote:
     "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma",
   streak: 3,
@@ -20,11 +22,10 @@ const leaderboardData = [
   { rank: 3, name: "Abdullah Ghazi", score: 94 },
   { rank: 4, name: "Abdullah Ashfaq", score: 83 },
   { rank: 5, name: "Etisam ul Haq", score: 80 },
-  { rank: 1543, name: "Etisam ul Haq", score: 80 },
+  { rank: 1543, name: username, score: 35 },
 ];
 
 function dashboard() {
-  const username = sessionStorage.getItem("username");
   return (
     <div>
        <div className="h-[300px] bg-white p-6 flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-6">
