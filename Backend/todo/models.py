2be2afca0baae7df1f6ced35341913830a5ab521
Abdullah Ashfaq <abdullahashfaq.ds@gmail.com>
2024-11-24 15:44:27 +0500

    Add ToDo User Session

--- Backend/todo/models.py
+++ Backend/todo/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from account.models import User
 
 
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     deadline = models.DateField()
     status = models.CharField(
         max_length=50,
         choices=[
-            ('Completed', 'Completed'), 
+            ('Completed', 'Completed'),
             ('Pending', 'Pending'),
             ('Not Completed', 'Not Completed')
         ],
